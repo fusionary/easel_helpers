aa4111cf7458f5ab6677675af8794a0e1b6f31f1
Joshua Clayton <jclayton@fusionary.com>
2009-05-04 09:03:06 -0400

    Added more tests for zebra_row

--- test/table_helper_test.rb
+++ test/table_helper_test.rb
@@ -5,11 +5,29 @@ class TableHelperTest < EaselHelpers::ViewTestCase
   context "zebra_row" do
     
     should "default with the correct structure" do
-      show_view "<% zebra_row do %>no class<% end %><% zebra_row do %>alt class<% end %>" do
-        assert_select "tr", 2
-        assert_select "tr.alt", 1
-        assert_select "tr", "no class"
-        assert_select "tr.alt", "alt class"
+      show_view "<table><% zebra_row do %>no class<% end %><% zebra_row do %>alt class<% end %></table>" do
+        assert_select "tr:first-child", "no class"
+        assert_select "tr.alt:last-child", "alt class"
+      end
+    end
+    
+    should "allow override of the cycle list" do
+      show_view %(
+        <table>
+          <% (colors = %w(red white blue)).each do |color| %>
+            <% zebra_row :cycle_list => colors do %>the color <%= color %><% end %>
+          <% end %>
+        </table>
+      ) do
+        assert_select "tr.red:first-child", "the color red"
+        assert_select "tr.white", "the color white"
+        assert_select "tr.blue:last-child", "the color blue"
+      end
+    end
+    
+    should "allow option assignment" do
+      show_view "<% zebra_row :id => 'my-id', :class => 'custom-class' do %>user 1<% end %>" do
+        assert_select "tr#my-id.custom-class", "user 1"
       end
     end
     
