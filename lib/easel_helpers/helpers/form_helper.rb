a3fdf0668044a994f328b8ff3b01e6aeb59b32a1
Joshua Clayton <jclayton@fusionary.com>
2009-06-18 23:42:42 -0400

    Code cleanup

--- lib/easel_helpers/helpers/form_helper.rb
+++ lib/easel_helpers/helpers/form_helper.rb
@@ -1,28 +1,40 @@
 module EaselHelpers
   module Helpers
     module FormHelper
-      
+
       def submit_button(value, *args)
         options = args.extract_options!
         css_classes = ["btn"] << options.delete(:class) << args
-        content_tag(:button, "<span>#{value}</span>", {:value => value, :type => "submit", :class => clean_css_classes(css_classes, {"last" => last_column})}.merge(options))
+        css_classes = clean_css_classes(css_classes, {"last" => last_column})
+        content_tag(:button, "<span>#{value}</span>", {
+                    :value => value, 
+                    :type => "submit", 
+                    :class => css_classes}.merge(options))
       end
-      
+
       def set(*args, &block)
         options = args.extract_options!
         css_classes = [] << options.delete(:class) << args
-        css_classes << "text" unless other_than_grid?(args.map(&:to_s) - ["last", last_column.to_s])
-        css_classes << "text" if standardize_css_classes(css_classes).include?("textarea")
-        
-        css_classes = clean_css_classes(css_classes, {"last" => last_column})
-        
-        html = clean_column(css_classes) do
-          content_tag(:div, capture(&block), options.merge(:class => css_classes))
+
+        unless other_than_grid?(args.map(&:to_s) - ["last", last_column.to_s])
+          css_classes << "text"
         end
-        
+
+        if standardize_css_classes(css_classes).include?("textarea")
+          css_classes << "text" 
+        end
+
+        css_classes = clean_css_classes(css_classes, {"last" => last_column})
+
+        html = clean_column(css_classes) do
+          content_tag(:div, 
+                      capture(&block), 
+                      options.merge(:class => css_classes))
+        end
+
         concat(html)
       end
-      
+
       def fieldset(*args, &block)
         options = args.extract_options!
         css_classes = [] << options.delete(:class) << args
@@ -31,20 +43,23 @@ module EaselHelpers
           ""
         else
           legend_opts = options.delete(:legend) || {}
+          legend_classes = clean_css_classes([legend_opts.delete(:class)] << "legend")
           content_tag(:h3, 
-            title, 
-            {:class => clean_css_classes([legend_opts.delete(:class)] << "legend")}.merge(legend_opts))
+                      title, 
+                      {:class => legend_classes}.merge(legend_opts))
         end
-        
+
         css_classes = clean_css_classes(css_classes, {"last" => last_column})
-        
+
         html = clean_column(css_classes) do
-          content_tag(:fieldset, legend + capture(&block), options.merge(:class => css_classes))
+          content_tag(:fieldset, 
+                      legend + capture(&block), 
+                      options.merge(:class => css_classes))
         end
-        
+
         concat(html)
       end
-      
+
     end
   end
 end
--- lib/easel_helpers/helpers/grid_helper.rb
+++ lib/easel_helpers/helpers/grid_helper.rb
@@ -28,25 +28,25 @@ module EaselHelpers
         :twentythree_twentyfourths => (23/24.to_f),
         :full =>                      (1.to_f)
       }.freeze
-      MULTIPLE_FRACTIONS = MULTIPLES.keys.map {|k| k.to_s }.freeze
-      
+      MULTIPLE_FRACTIONS = MULTIPLES.keys.map {|key| key.to_s }.freeze
+
       def last_column
         "col-last"
       end
-      
+
       def column(*args, &block)
         @_easel_column_count ||= application_width
         col(*args, &block)
       end
-      
+
       def container(size=nil, *args, &block)
         args = args.insert(0, :container)
         column(size, *args, &block)
       end
-      
+
       def clean_column(classes, &block)
         size = classes.scan(/col-(\d+)/).flatten.last
-        
+
         if size.nil?
           html = capture(&block)
           if block_given? && block_is_within_action_view?(block)
@@ -58,7 +58,7 @@ module EaselHelpers
           size = size.to_i
           increase_depth(size)
           html = capture(&block)
-          
+
           if block_given? && block_is_within_action_view?(block)
             concat(html)
             decrease_depth(size)
@@ -68,20 +68,21 @@ module EaselHelpers
           end
         end
       end
-      
+
       def method_missing_with_easel_widths(call, *args)
         # filter out any initial helper calls
         found = false
-        MULTIPLE_FRACTIONS.each do |w|
-          found = true and break if call.to_s.include?(w)
+        MULTIPLE_FRACTIONS.each do |fraction|
+          found = true and break if call.to_s.include?(fraction)
         end
+
         method_missing_without_easel_widths(call, *args) and return unless found
-        
+
         # one of the widths is somewhere in the helper call; let's find it
         call.to_s =~ /^((append|prepend|col)_)?(.+)$/
         class_name = $2 || "col"
         class_width = $3
-        
+
         if MULTIPLES.keys.include?(class_width.to_sym)
           width = @_easel_column_count || application_width
           "#{class_name}-#{(width*MULTIPLES[class_width.to_sym]).to_i}"
@@ -89,30 +90,44 @@ module EaselHelpers
           method_missing_without_easel_widths(call, *args)
         end
       end
-      
+
       alias_method_chain :method_missing, :easel_widths
-      
+
       private
-      
+
       def application_width; 24; end
-      
+
       def increase_depth(size)
         @_easel_current_width ||= [application_width.to_s]
-        @_easel_current_width.push @_easel_column_count.to_s unless @_easel_column_count.blank?
-        @_easel_column_count = size.to_s =~ /^\d+$/ ? size.to_s.to_i : (@_easel_column_count*MULTIPLES[size]).to_i
+        unless @_easel_column_count.blank?
+          @_easel_current_width.push(@_easel_column_count.to_s)
+        end
+
+        @_easel_column_count = if size.to_s =~ /^\d+$/
+          size.to_s.to_i
+        else
+          (@_easel_column_count*MULTIPLES[size]).to_i
+        end
       end
-      
+
       def decrease_depth(size)
-        @_easel_column_count = size.is_a?(Integer) ? @_easel_current_width.last.to_i : (@_easel_column_count/MULTIPLES[size]).to_i
+        @_easel_column_count = if size.is_a?(Integer)
+          @_easel_current_width.last.to_i
+        else
+          (@_easel_column_count/MULTIPLES[size]).to_i
+        end
+
         @_easel_current_width.pop
       end
-      
+
       def col(*args, &block)
-        size = (MULTIPLE_FRACTIONS.include?(args.first.to_s) || args.first.is_a?(Integer)) ? args.shift : :full
-        
+        fractional_width = MULTIPLE_FRACTIONS.include?(args.first.to_s)
+        explicit_column_width = args.first.is_a?(Integer)
+        size = (fractional_width || explicit_column_width) ? args.shift : :full
+
         increase_depth(size)
         output_tag = generate_output_tag(size, *args, &block)
-        
+
         if block_given? && block_is_within_action_view?(block)
           concat(output_tag)
           decrease_depth(size)
@@ -121,17 +136,25 @@ module EaselHelpers
           output_tag
         end
       end
-      
+
       def generate_output_tag(size, *args, &block)
         options = args.extract_options!
-        
+
         css_classes = [] << options.delete(:class) << args
-        css_classes << "col-#{@_easel_column_count}" unless options.delete(:suppress_col)
-        css_classes << last_column if size.to_sym == :full && @_easel_column_count != application_width
-        
-        content_tag(:div, capture(&block), {:class => clean_css_classes(css_classes, {"last" => last_column})}.merge(options))
+        unless options.delete(:suppress_col)
+          css_classes << "col-#{@_easel_column_count}"
+        end
+
+        if size.to_sym == :full && @_easel_column_count != application_width
+          css_classes << last_column
+        end
+
+        css_classes = clean_css_classes(css_classes, {"last" => last_column})
+        content_tag(:div, 
+                    capture(&block), 
+                    {:class => css_classes}.merge(options))
       end
-      
+
     end
   end
 end
