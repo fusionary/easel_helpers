3800950f81fda1ed247147f120a88d04c7890a9e
Joshua Clayton <jclayton@fusionary.com>
2009-07-22 08:26:31 -0400

    More tweaks to navigation helper and its tests

--- lib/easel_helpers/helpers/navigation_helper.rb
+++ lib/easel_helpers/helpers/navigation_helper.rb
@@ -3,24 +3,24 @@ module EaselHelpers
     module NavigationHelper
       def tab(name, path, options = {}, li_options = {})
         opts = parse_tab_options(name, li_options)
-        active, comparison, compare, li_classes = \
-          opts[:active], opts[:comparison], opts[:compare], opts[:li_classes]
 
-        css_classes = [] << li_classes
-        css_classes << "active" if (active == comparison) || compare
+        active = "active" if (opts[:active] == opts[:comparison]) || opts[:compare]
+        css_classes = [] << opts[:li_classes] << active
         css_classes = clean_css_classes(css_classes)
+        li_options.merge!(:class => css_classes) unless css_classes.blank?
+
         content_tag :li,
                     link_to(name, path, options),
-                    li_options.merge(:class => (css_classes.blank? ? nil : css_classes))
+                    li_options
       end
 
       private
 
       def parse_tab_options(name, li_options = {})
         returning({}) do |result|
-          result[:active]     = li_options.delete(:active) || (name.gsub(/\s/, '').tableize || "")
+          result[:active]     = li_options.delete(:active)         || (name.gsub(/\s/, '').tableize || "")
           result[:comparison] = li_options.delete(:active_compare) || controller.controller_name
-          result[:compare]    = li_options.delete(:compare) || false
+          result[:compare]    = li_options.delete(:compare)        || false
           result[:li_classes] = li_options.delete(:class)
         end
       end
--- test/navigation_helper_test.rb
+++ test/navigation_helper_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class NavigationHelperTest < ActiveSupport::TestCase
-  include EaselHelpers::Helpers
   include EaselHelpers::Helpers::NavigationHelper
 
   context "tab" do
@@ -9,24 +8,31 @@ class NavigationHelperTest < ActiveSupport::TestCase
       expects(:parse_tab_options).with("test", {:b => 2}).returns({})
       stubs(:link_to)
       stubs(:content_tag)
+      stubs(:clean_css_classes)
       tab("test", "/", {:a => 1}, {:b => 2})
+    end
+
+    should "call link_to properly" do
+      stubs(:content_tag)
+      stubs(:clean_css_classes)
+      stubs(:parse_tab_options).returns({})
+      expects(:link_to).with("test", "/path", {:a => 1, :b => 2})
+      tab("test", "/path", {:a => 1, :b => 2})
     end
 
     context "compiling li classes" do
       setup do
-        @result_options = {
-                            :active => "1",
+        @result_options = { :active => "1",
                             :comparison => "2",
                             :compare => false,
-                            :li_classes => "one two three"
-                          }
-        stubs(:link_to)
+                            :li_classes => "one two three" }
+        stubs(:link_to).returns("link_to")
         stubs(:content_tag)
       end
 
       should "properly use classes passed in the options" do
         stubs(:parse_tab_options).returns(@result_options)
-        expects(:clean_css_classes).with(["one two three"])
+        expects(:clean_css_classes).with(["one two three", nil])
         tab("test", "/")
       end
 
@@ -41,6 +47,14 @@ class NavigationHelperTest < ActiveSupport::TestCase
         @result_options[:compare] = true
         stubs(:parse_tab_options).returns(@result_options)
         expects(:clean_css_classes).with(["one two three", "active"])
+        tab("test", "/")
+      end
+
+      should "not assign class if css_classes is blank" do
+        @result_options[:li_classes] = nil
+        stubs(:parse_tab_options).returns({})
+        stubs(:clean_css_classes).returns("")
+        expects(:content_tag).with(:li, "link_to", {})
         tab("test", "/")
       end
     end
